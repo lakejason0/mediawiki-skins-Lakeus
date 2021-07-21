e7e199a99e406289981546a21cfa6c758f9d9cf1
lakejason0 <sunliyuan200402@outlook.com>
2021-07-21 20:15:18 +0800

    Portlet would close when clicking outside

--- resources/skin.js
+++ resources/skin.js
@@ -1,9 +1,17 @@
 /* scripts can go here */
-$( function (){
-    $(document).on('click', function(e) {
-        var portletMenuList = $(".mw-portlet-body ul");
-        if (!$(e.target).closest(portletMenuList).length) {
-            $(".mw-portlet input[type=checkbox]").prop('checked', false);
-        }
-    });
-} );
+$(function () {
+    function portletOutsideClose(id) {
+        $(document).on('click', function (event) {
+            var $target = $(event.target);
+            if ((!$target.closest(id + ' .mw-portlet-body ul').length ||
+                !$target.closest(id + ' .mw-portlet input[type="checkbox"]').length) &&
+                ($(id + ' .mw-portlet-body').css("opacity") == 1)) {
+                $(id + ' input[type="checkbox"]').prop('checked', false);
+            }
+        })
+    }
+    portletOutsideClose('#p-personal');
+    portletOutsideClose('#p-cactions');
+    portletOutsideClose('#p-lang');
+    portletOutsideClose('#p-variants');
+});
