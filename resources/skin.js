d1d999820bebe8a6403b0c04e61e3ff472db406a
lakejason0 <sunliyuan200402@outlook.com>
2021-07-21 16:46:55 +0800

    Update

--- resources/skin.js
+++ resources/skin.js
@@ -1,1 +1,9 @@
 /* scripts can go here */
+$( function (){
+    $(document).on('click', function(e) {
+        var portletMenuList = $(".mw-portlet-body ul");
+        if (!$(e.target).closest(portletMenuList).length) {
+            $(".mw-portlet input[type=checkbox]").prop('checked', false);
+        }
+    });
+} );
