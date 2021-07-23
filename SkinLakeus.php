c3b7de96a369e7ce10028c2a73bbd4fe416cee1d
lakejason0 <sunliyuan200402@outlook.com>
2021-07-23 13:19:40 +0800

    Extending data

---
+++ SkinLakeus.php
@@ -0,0 +1,19 @@
+<?php
+
+use SiteStats;
+
+class SkinLakeus extends SkinMustache {
+    /**
+     * Extends the getTemplateData function to add a template key 'html-myskin-hello-world'
+     * which can be rendered in skin.mustache using {{{html-myskin-hello-world}}}
+     */
+    public function getTemplateData() {
+        $data = parent::getTemplateData();
+
+        $data["html-articlecount"] = SiteStats::articles();
+        // trying to overwrite
+        $data["msg-lakeus-articlecount"] = wfMessage( 'lakeus-articlecount' )->numParams( SiteStats::articles() )->plain();
+
+        return $data;
+    }
+}
