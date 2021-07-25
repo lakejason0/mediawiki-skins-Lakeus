6774187a1de6328b4906923f2a212c86a61e69d2
lakejason0 <sunliyuan200402@outlook.com>
2021-07-25 15:37:53 +0800

    Add a footer message

--- SkinLakeus.php
+++ SkinLakeus.php
@@ -9,11 +9,15 @@ class SkinLakeus extends SkinMustache {
      */
     public function getTemplateData() {
         $data = parent::getTemplateData();
+        $config = $this->getConfig(); // this is a Config object
 
         $data["html-articlecount"] = SiteStats::articles();
         // trying to overwrite
         $data["msg-lakeus-articlecount"] = wfMessage( 'lakeus-articlecount' )->numParams( SiteStats::articles() )->parse();
-
+        $wgLakeusShowRepositoryLink = $config->get( 'LakeusShowRepositoryLink' );
+        if ($wgLakeusShowRepositoryLink) {
+            $data["html-repository-link"] = '<a href="https://github.com/lakejason0/mediawiki-skins-Lakeus">' .. wfMessage( 'lakeus-footermessage' )->plain() .. '</a>'; 
+        }
         return $data;
     }
 }
