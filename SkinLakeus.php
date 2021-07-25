3119d6a6124a30c8beff55393932bfa1aa847658
lakejason0 <sunliyuan200402@outlook.com>
2021-07-25 15:49:11 +0800

    Fix a comment

--- SkinLakeus.php
+++ SkinLakeus.php
@@ -5,7 +5,7 @@ use SiteStats;
 class SkinLakeus extends SkinMustache {
     /**
      * Extends the getTemplateData function to add a template key 'html-myskin-hello-world'
-     * which can be rendered in skin.mustache using {{html-myskin-hello-world}}
+     * which can be rendered in skin.mustache using {{{html-myskin-hello-world}}}
      */
     public function getTemplateData() {
         $data = parent::getTemplateData();
