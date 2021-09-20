48f3f2ca17db90581f2e0aa58975e44f1a899757
lakejason0 <sunliyuan200402@outlook.com>
2021-09-20 11:13:32 +0800

    Add modules to skin.json and hook for search vue
    Rebased from commit "Update" b82e2a48a699c6612b11c183cfb6376323a3c6bb
    
    Bug: #21
    Change-Id: Icac717f530a63e11b0e5ab20892b499abc5d1251

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,9 +2,23 @@
 
 namespace MediaWiki\Skins\Lakeus;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Preferences\Hook\GetPreferencesHook;
+use MediaWiki\ResourceLoader\Context as ResourceLoaderContext;
 
 class Hooks implements GetPreferencesHook {
+	/**
+	 * @param ResourceLoaderContext $context
+	 * @param Config $config
+	 * @return $wgLakeusWvuiSearchOptions
+	 */
+	public function getSnapwikiskinWvuiSearchResourceLoaderConfig(
+		ResourceLoaderContext $context,
+		Config $config
+	) {
+		return $config->get( 'LakeusWvuiSearchOptions' );
+	}
+
 	/**
 	 * @see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Hooks/GetPreferences
 	 * @param User $user
