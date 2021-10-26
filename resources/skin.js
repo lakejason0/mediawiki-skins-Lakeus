edf30d0427dcfa1a2c2a735263fbd5d56317eef7
lakejason0 <sunliyuan200402@outlook.com>
2021-10-27 00:09:17 +0800

    Semantic variables

--- resources/skin.js
+++ resources/skin.js
@@ -1,17 +1,18 @@
 /* scripts can go here */
+window.Lakeus = window.Lakeus || {};
+Lakeus.portletOutsideClose = function (id) {
+    $(document).on('click', function (event) {
+        var $target = $(event.target);
+        if ((!$target.closest(id + ' .mw-portlet-body ul').length ||
+            !$target.closest(id + ' .mw-portlet input[type="checkbox"]').length) &&
+            ($(id + ' .mw-portlet-body').css("opacity") == 1)) {
+            $(id + ' input[type="checkbox"]').prop('checked', false);
+        }
+    })
+}
 $(function () {
-    function portletOutsideClose(id) {
-        $(document).on('click', function (event) {
-            var $target = $(event.target);
-            if ((!$target.closest(id + ' .mw-portlet-body ul').length ||
-                !$target.closest(id + ' .mw-portlet input[type="checkbox"]').length) &&
-                ($(id + ' .mw-portlet-body').css("opacity") == 1)) {
-                $(id + ' input[type="checkbox"]').prop('checked', false);
-            }
-        })
-    }
-    portletOutsideClose('#p-personal');
-    portletOutsideClose('#p-cactions');
-    portletOutsideClose('#p-lang');
-    portletOutsideClose('#p-variants');
+    Lakeus.portletOutsideClose('#p-personal');
+    Lakeus.portletOutsideClose('#p-cactions');
+    Lakeus.portletOutsideClose('#p-lang');
+    Lakeus.portletOutsideClose('#p-variants');
 });
---
+++ resources/themeDesigner.js
@@ -0,0 +1,1 @@
+window.Lakeus = window.Lakeus || {};
