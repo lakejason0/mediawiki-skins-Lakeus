5b915f1a4c752996c767a4d04ac915910d5a7793
lakejason0 <sunliyuan200402@outlook.com>
2021-10-27 00:51:46 +0800

    Update

--- resources/themeDesigner.js
+++ resources/themeDesigner.js
@@ -1,1 +1,9 @@
 window.Lakeus = window.Lakeus || {};
+
+Lakeus.initThemeDesigner = function () {
+    console.log("[Lakeus] Theme Designer is loading...");
+    var stylePath = mw.config.get('stylepath');
+    $('head').append('<link rel="stylesheet" href="' + stylePath + '/Lakeus/resources/themeDesigner.css' + '" type="text/css" />');
+    $("body").append('<div id="lakeus-theme-designer"><button id="lakeus-theme-designer-modal-button">🖌️</button></div>');
+    console.log("[Lakeus] Theme Designer is loaded.");
+};
