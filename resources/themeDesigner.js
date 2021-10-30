33c0d87b3cb4f50f3718bd7bbba387f6bf6cfe71
lakejason0 <sunliyuan200402@outlook.com>
2021-10-30 14:26:26 +0800

    Update

--- resources/themeDesigner.js
+++ resources/themeDesigner.js
@@ -21,13 +21,13 @@ Lakeus.initThemeDesigner = function () {
         return rgb;
     }
 
-    Lakeus.getContrastYIQ = function (hexcolor) {
+    Lakeus.getContrastYIQ = function (hexcolor, black, white) {
         hexcolor = hexcolor.replace("#", "");
         var r = parseInt(hexcolor.substr(0, 2), 16);
         var g = parseInt(hexcolor.substr(2, 2), 16);
         var b = parseInt(hexcolor.substr(4, 2), 16);
         var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-        return (yiq >= 128) ? 'black' : 'white';
+        return (yiq >= 128) ? (black ? black : '#000000') : (white ? white : '#ffffff');
     }
 
     Lakeus.variablesList = {
@@ -452,10 +452,13 @@ Lakeus.initThemeDesigner = function () {
                         '<div id="lakeus-theme-designer-portlet-body-container">' +
                             '<h1 id="lakeus-theme-designer-title">' + mw.message("lakeus-theme-designer") + '</h1>' +
                             constructThemeDesignerBody() +
+                            '<div id="lakeus-theme-designer-result">' +
+                            '</div>' +
                             '<div id="lakeus-theme-designer-action-buttons">' +
                                 '<button id="lakeus-theme-designer-copy-theme-button" class="lakeus-theme-designer-action-button">' + mw.message("lakeus-theme-designer-copy-theme") +
                                 '<button id="lakeus-theme-designer-test-theme-button" class="lakeus-theme-designer-action-button">' + mw.message("lakeus-theme-designer-test-theme") +
                                 '<button id="lakeus-theme-designer-paste-theme-button" class="lakeus-theme-designer-action-button">' + mw.message("lakeus-theme-designer-paste-theme") +
+                                '<button disabled id="lakeus-theme-designer-clear-theme-button" class="lakeus-theme-designer-action-button">' + mw.message("lakeus-theme-designer-clear-theme") +
                             '</div>' +
                         '</div>' +
                     '</form>' +
@@ -469,6 +472,7 @@ Lakeus.initThemeDesigner = function () {
         $("#lakeus-theme-designer-copy-theme-button").click( function(e) { e.preventDefault; Lakeus.copyTheme();  });
         $("#lakeus-theme-designer-paste-theme-button").click( function(e) { e.preventDefault; Lakeus.pasteThemeFromCurrentSettings();  });
         $("#lakeus-theme-designer-test-theme-button").click( function(e) { e.preventDefault; Lakeus.testTheme();  });
+        $("#lakeus-theme-designer-clear-theme-button").click( function(e) { e.preventDefault; Lakeus.clearTheme();  });
         Lakeus.updateVariablesListFromForm();
     }
 
@@ -482,6 +486,9 @@ Lakeus.initThemeDesigner = function () {
             "lakeus-theme-designer-copy-theme",
             "lakeus-theme-designer-paste-theme",
             "lakeus-theme-designer-test-theme",
+            "lakeus-theme-designer-clear-theme",
+            "lakeus-theme-designer-copied",
+            "lakeus-theme-designer-copy-failed",
             "lakeus-theme-designer-global",
             "lakeus-theme-designer-background-color-base",
             "lakeus-theme-designer-color-link",
@@ -538,22 +545,110 @@ Lakeus.updateVariablesListFromForm = function () {
         var inputElement = $("#lakeus-theme-designer-input-" + k);
         if (!(inputElement.prop('disable'))) {
             Lakeus.variablesList[k].value = inputElement.val();
+        } else {
+            Lakeus.variablesList[k].value = v.calculate(undefined);
         }
     });
 };
 
-Lakeus.generateTheme = function () {
+Lakeus.updateFormFromVariablesList = function () {
+    $.each(Lakeus.variablesList, function (k, v) {
+        var inputElement = $("#lakeus-theme-designer-input-" + k);
+        if (v.value) {
+            inputElement.val(v.value);
+        }
+    })
+}
 
+Lakeus.generateTheme = function () {
+    var result = ':root {\n';
+    $.each(Lakeus.variablesList, function (k, v) {
+        result += '    ' + '--' + k + ': ' + v.value + ';\n';
+    });
+    result += '}\n';
+    return result;
 }
 
 Lakeus.copyTheme = function () {
-
+    generatedCode = Lakeus.generateTheme();
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(generatedCode).then(function() {
+            mw.notify(mw.message("lakeus-theme-designer-copied"));
+        }, function () {
+            mw.notify(mw.message("lakeus-theme-designer-copy-failed"));
+            $("lakeus-theme-designer-result").empty().append(
+                '<pre id="lakeus-theme-designer-result-pre-element">' +
+                    generatedCode +
+                '</pre>'
+            );
+        });
+    } else {
+        mw.notify(mw.message("lakeus-theme-designer-copy-failed"));
+        $("lakeus-theme-designer-result").empty().append(
+            '<pre id="lakeus-theme-designer-result-pre-element">' +
+                generatedCode +
+            '</pre>'
+        );
+    }
 }
 
 Lakeus.pasteThemeFromCurrentSettings = function () {
-
+    $.each(Lakeus.variablesList, function (k, v) {
+        if (v.input === 'color') {
+            Lakeus.variablesList[k].value = Lakeus.colorNameToHex(window.getComputedStyle(document.querySelector('html')).getPropertyValue("--" + k));
+        } else {
+            Lakeus.variablesList[k].value = window.getComputedStyle(document.querySelector('html')).getPropertyValue("--" + k);
+        }
+    })
+    Lakeus.updateFormFromVariablesList();
 }
 
 Lakeus.testTheme = function () {
+    $("body").attr("testing", "true");
+    $("#lakeus-theme-designer-test-theme-button").prop('disabled', true);
+    $("#lakeus-theme-designer-clear-theme-button").prop('disabled', false);
+    $.each(Lakeus.variablesList, function (k, v) {
+        document.querySelector('html').style.setProperty("--" + k, v.value);
+    });
+}
 
+Lakeus.clearTheme = function () {
+    $("body").attr("testing", "false");
+    $("#lakeus-theme-designer-test-theme-button").prop('disabled', false);
+    $("#lakeus-theme-designer-clear-theme-button").prop('disabled', true);
+    $.each(Lakeus.variablesList, function (k, v) {
+        document.querySelector('html').style.removeProperty("--" + k);
+    });
+}
+
+Lakeus.colorNameToHex = function(color) {
+    var colors = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
+    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
+    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
+    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
+    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
+    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
+    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
+    "honeydew":"#f0fff0","hotpink":"#ff69b4",
+    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
+    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
+    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
+    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
+    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
+    "navajowhite":"#ffdead","navy":"#000080",
+    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
+    "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
+    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
+    "violet":"#ee82ee",
+    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
+    "yellow":"#ffff00","yellowgreen":"#9acd32"};
+
+    if (typeof colors[color.toLowerCase()] != 'undefined')
+        return colors[color.toLowerCase()];
+
+    return color;
 }
