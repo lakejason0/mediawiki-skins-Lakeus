e1ee98c7344843d193e337c8afb7eb1e2ed6caa4
lakejason0 <sunliyuan200402@outlook.com>
2021-11-05 00:56:14 +0800

    Update

--- resources/themeDesigner.js
+++ resources/themeDesigner.js
@@ -603,7 +603,7 @@ Lakeus.initThemeDesigner = function () {
                     '<label>' +
                         mw.message('lakeus-theme-designer-' + variableName) +
                         '<input type="color" name="' + variableName + '" id="lakeus-theme-designer-input-' + variableName + '" value="' + variableContent.default + '" />' +
-                        '<input type="number" step="0.01" min="0" max="1" name="' + variableName + '-alpha" id="lakeus-theme-designer-input-' + variableName + '-alpha" value="' + variableContent.alpha() + '" />' +
+                        '<input type="number" step="0.01" min="0" max="1" name="' + variableName + '-alpha" id="lakeus-theme-designer-input-' + variableName + '-alpha" value="' + variableContent.default.alpha() + '" />' +
                     '</label>';
             } else if (variableContent.input === 'text') {
                 settingElement +=
