0f0aa1195ac8ff6702fcc4266684218ae8524582
Winston Sung <winston21.sung@gmail.com>
2023-12-22 16:04:00 +0800

    chore: Clean-up and tests
    * Namespace
    * PHPCS
    * Screenshot

--- includes/SkinLakeus.php
+++ includes/SkinLakeus.php
@@ -1,48 +1,69 @@
 <?php
+
+namespace MediaWiki\Skins\Lakeus;
+
+use BagOStuff;
+# use MediaWiki\Html\TemplateParser; // Namespaced in 1.40.0
+# use MediaWiki\SiteStats\SiteStats; // Namespaced in 1.41.0
+use SiteStats;
+use SkinMustache;
+use TemplateParser;
+
 class SkinLakeus extends SkinMustache {
 
-    public const TEMPLATE_DIR = __DIR__ . '/templates';
+	public const TEMPLATE_DIR = __DIR__ . '/templates';
 
-    /**
-     * @param BagOStuff $localServerObjectCache
-     * @param array $options
-     */
-    public function __construct( BagOStuff $localServerObjectCache, array $options ) {
-        $options['templateDirectory'] = self::TEMPLATE_DIR;
-        parent::__construct( $options );
+	/**
+	 * @param BagOStuff $localServerObjectCache
+	 * @param array $options
+	 */
+	public function __construct( BagOStuff $localServerObjectCache, array $options ) {
+		$options['templateDirectory'] = self::TEMPLATE_DIR;
+		parent::__construct( $options );
 
-        $this->templateParser = new TemplateParser( $this->options['templateDirectory'], $localServerObjectCache );
-    }
+		$this->templateParser = new TemplateParser( $this->options['templateDirectory'], $localServerObjectCache );
+	}
 
-    /**
-     * Extends the getTemplateData function to add a template key 'html-myskin-hello-world'
-     * which can be rendered in skin.mustache using {{{html-myskin-hello-world}}}
-     */
-    public function getTemplateData() {
-        $data = parent::getTemplateData();
-        $config = $this->getConfig(); // this is a Config object
+	/**
+	 * Extends the getTemplateData function to add a template key 'html-myskin-hello-world'
+	 * which can be rendered in skin.mustache using {{{html-myskin-hello-world}}}
+	 *
+	 * @return array
+	 */
+	public function getTemplateData(): array {
+		$data = parent::getTemplateData();
+		// this is a Config object
+		$config = $this->getConfig();
 
-        $data["html-articlecount"] = SiteStats::articles();
-        // trying to overwrite
-        $data["msg-lakeus-articlecount"] = $this->msg( 'lakeus-articlecount' )->numParams( SiteStats::articles() )->parse();
-        $wgLakeusShowRepositoryLink = $config->get( 'LakeusShowRepositoryLink' );
-        if ($wgLakeusShowRepositoryLink) {
-            $data["html-repository-link"] = '<a href="https://github.com/lakejason0/mediawiki-skins-Lakeus">' . $this->msg( 'lakeus-footermessage' )->plain() . '</a>'; 
-            $data["is-repository-link-shown"] = true;
-        }
-        $data["is-notice-with-border"] = $config->get( 'LakeusSiteNoticeHasBorder' );
-        $data["is-portlet-animated"] = $config->get( 'LakeusShouldAnimatePortlets' );
-        $data["is-sticky-toc-shown"] = $config->get( 'LakeusShowStickyTOC' );
+		$data["html-articlecount"] = SiteStats::articles();
+		// trying to overwrite
+		$data["msg-lakeus-articlecount"] = $this->msg( 'lakeus-articlecount' )
+			->numParams( SiteStats::articles() )->parse();
+		$showRepositoryLink = $config->get( 'LakeusShowRepositoryLink' );
+		if ( $showRepositoryLink ) {
+			$data["html-repository-link"] = '<a href="https://github.com/lakejason0/mediawiki-skins-Lakeus">' .
+				$this->msg( 'lakeus-footermessage' )->plain() . '</a>';
+			$data["is-repository-link-shown"] = true;
+		}
+		$data["is-notice-with-border"] = $config->get( 'LakeusSiteNoticeHasBorder' );
+		$data["is-portlet-animated"] = $config->get( 'LakeusShouldAnimatePortlets' );
+		$data["is-sticky-toc-shown"] = $config->get( 'LakeusShowStickyTOC' );
 
-        $pageToolsKey = array_search('p-tb' , array_column( $data["data-portlets-sidebar"]["array-portlets-rest"], 'id' ) );
-        $data["data-portlets"]["data-page-tools"] = $data["data-portlets-sidebar"]["array-portlets-rest"][$pageToolsKey];
-        unset($data["data-portlets-sidebar"]["array-portlets-rest"][$pageToolsKey]);
-        $data["data-portlets-sidebar"]["array-portlets-rest"] = array_values($data["data-portlets-sidebar"]["array-portlets-rest"]);
+		$pageToolsKey = array_search(
+			'p-tb',
+			array_column( $data["data-portlets-sidebar"]["array-portlets-rest"], 'id' )
+		);
+		$data["data-portlets"]["data-page-tools"] =
+			$data["data-portlets-sidebar"]["array-portlets-rest"][$pageToolsKey];
+		unset( $data["data-portlets-sidebar"]["array-portlets-rest"][$pageToolsKey] );
+		$data["data-portlets-sidebar"]["array-portlets-rest"] = array_values(
+			$data["data-portlets-sidebar"]["array-portlets-rest"]
+		);
 
-        if ( empty($data["data-toc"]["array-sections"]) ) {
-            unset($data["data-toc"]);
-        }
+		if ( empty( $data["data-toc"]["array-sections"] ) ) {
+			unset( $data["data-toc"] );
+		}
 
-        return $data;
-    }
+		return $data;
+	}
 }
