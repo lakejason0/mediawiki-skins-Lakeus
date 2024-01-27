2dfcc0a5f5a07c9a7e0481f08305b3f50357c409
Winston Sung <winston21.sung@gmail.com>
2024-01-27 22:28:21 +0800

    Migrate templateDirectory option to skin.json
    See: mediawiki/core 1.35.0
    Change-Id Ibbabd1d0f26efebf8f8ff068966685dc2191c527
    (commit 26d5f78f84d57fca007700f143b2c51ab8840d69 )
    See mediawiki/skins/Vector
    Change-Id Ieccdf87979d14eeec0834b6b0cecf064d5fd9cfc
    (commit 3435f8fd9b3cc5a38560ed75a6d8440df4e1cd54 )
    
    Bug: T262067
    Bug: #33
    Change-Id: I8eedbebbf1782bfcfbd35e4ed113e9a71c11e0ff

--- includes/SkinLakeus.php
+++ includes/SkinLakeus.php
@@ -10,15 +10,11 @@ use SkinMustache;
 use TemplateParser;
 
 class SkinLakeus extends SkinMustache {
-
-	public const TEMPLATE_DIR = __DIR__ . '/templates';
-
 	/**
 	 * @param BagOStuff $localServerObjectCache
 	 * @param array $options
 	 */
 	public function __construct( BagOStuff $localServerObjectCache, array $options ) {
-		$options['templateDirectory'] = self::TEMPLATE_DIR;
 		parent::__construct( $options );
 
 		$this->templateParser = new TemplateParser( $this->options['templateDirectory'], $localServerObjectCache );
