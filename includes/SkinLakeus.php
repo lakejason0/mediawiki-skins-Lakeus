7b6877a2ccae3d51cd72b2569399932046639a56
Winston Sung <winston21.sung@gmail.com>
2024-09-22 14:08:10 +0800

    Update logic and styles of mw-portlet-dropdown
    Bug: #48
    Change-Id: Ic4cdacdd809eb5eaaba607134f315524dfcb2196

--- includes/SkinLakeus.php
+++ includes/SkinLakeus.php
@@ -18,44 +18,61 @@ class SkinLakeus extends SkinMustache {
 		// this is a Config object
 		$config = $this->getConfig();
 
-		$data["html-articlecount"] = SiteStats::articles();
+		$data['html-articlecount'] = SiteStats::articles();
 		// trying to overwrite
-		$data["msg-lakeus-articlecount"] = $this->msg( 'lakeus-articlecount' )
+		$data['msg-lakeus-articlecount'] = $this->msg( 'lakeus-articlecount' )
 			->numParams( SiteStats::articles() )->parse();
-		$showRepositoryLink = $config->get( 'LakeusShowRepositoryLink' );
-		if ( $showRepositoryLink ) {
-			$data["html-repository-link"] = '<a href="https://github.com/lakejason0/mediawiki-skins-Lakeus">' .
+		$show_repository_link = $config->get( 'LakeusShowRepositoryLink' );
+		if ( $show_repository_link ) {
+			$data['html-repository-link'] = '<a href="https://github.com/lakejason0/mediawiki-skins-Lakeus">' .
 				$this->msg( 'lakeus-footermessage' )->plain() . '</a>';
-			$data["is-repository-link-shown"] = true;
+			$data['is-repository-link-shown'] = true;
 		}
-		$data["is-notice-with-border"] = $config->get( 'LakeusSiteNoticeHasBorder' );
-		$data["is-portlet-animated"] = $config->get( 'LakeusShouldAnimatePortlets' );
-		$data["is-sticky-toc-shown"] = $config->get( 'LakeusShowStickyTOC' );
+		$data['is-notice-with-border'] = $config->get( 'LakeusSiteNoticeHasBorder' );
+		$data['is-portlet-animated'] = $config->get( 'LakeusShouldAnimatePortlets' );
+		$data['is-sticky-toc-shown'] = $config->get( 'LakeusShowStickyTOC' );
 
-		$pageToolsKey = array_search(
+		$page_tools_key = array_search(
 			'p-tb',
-			array_column( $data["data-portlets-sidebar"]["array-portlets-rest"], 'id' )
+			array_column( $data['data-portlets-sidebar']['array-portlets-rest'], 'id' )
 		);
-		$data["data-portlets"]["data-page-tools"] =
-			$data["data-portlets-sidebar"]["array-portlets-rest"][$pageToolsKey];
-		unset( $data["data-portlets-sidebar"]["array-portlets-rest"][$pageToolsKey] );
-		$data["data-portlets-sidebar"]["array-portlets-rest"] = array_values(
-			$data["data-portlets-sidebar"]["array-portlets-rest"]
+		$data['data-portlets']['data-page-tools'] =
+			$data['data-portlets-sidebar']['array-portlets-rest'][$page_tools_key];
+		unset( $data['data-portlets-sidebar']['array-portlets-rest'][$page_tools_key] );
+		$data['data-portlets-sidebar']['array-portlets-rest'] = array_values(
+			$data['data-portlets-sidebar']['array-portlets-rest']
 		);
 
-		if ( empty( $data["data-toc"]["array-sections"] ) ) {
-			unset( $data["data-toc"] );
+		$dropdown_portlet_keys = [
+			'data-user-menu',
+			'data-actions',
+			'data-page-tools',
+			'data-variants',
+			'data-languages',
+		];
+		foreach ( $dropdown_portlet_keys as $portlet_key ) {
+			if ( isset( $data['data-portlets'][$portlet_key] ) ) {
+				$data['data-portlets'][$portlet_key]['class'] = str_replace(
+					'mw-portlet mw-portlet-',
+					'mw-portlet mw-portlet-dropdown mw-portlet-',
+					$data['data-portlets'][$portlet_key]['class']
+				);
+			}
 		}
 
-		$customIndicators = $config->get( 'LakeusCustomIndicators' );
-		$idTracker = [];
-		foreach ( $data["array-indicators"] as $item ) {
-			$idTracker[$item["id"]] = true;
+		if ( empty( $data['data-toc']['array-sections'] ) ) {
+			unset( $data['data-toc'] );
 		}
-		foreach ( $customIndicators as $indicator ) {
-			if ( !isset( $idTracker[ $indicator["id"] ] ) ) {
-				$data["array-indicators"][] = $indicator;
-				$idTracker[ $indicator["id"] ] = true;
+
+		$custom_indicators = $config->get( 'LakeusCustomIndicators' );
+		$id_tracker = [];
+		foreach ( $data['array-indicators'] as $item ) {
+			$id_tracker[$item['id']] = true;
+		}
+		foreach ( $custom_indicators as $indicator ) {
+			if ( !isset( $id_tracker[ $indicator['id'] ] ) ) {
+				$data['array-indicators'][] = $indicator;
+				$id_tracker[ $indicator['id'] ] = true;
 			}
 		}
 
